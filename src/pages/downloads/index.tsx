3d21c539e12133e81177ea9d8d3e3f21e7d91460
Satyen-Sen <Satyendra.sant7@gmail.com>
2023-09-04 20:41:11 +0530

    empty pages created

---
+++ src/pages/downloads/index.tsx
@@ -0,0 +1,28 @@
+import { Box, Container, Typography } from '@mui/material'
+import Navbar from '../../components/Navbar'
+import FooterSection from '../../sections/FooterSection'
+import { useTheme } from '@mui/material/styles'
+
+export default function Home() {
+    const theme = useTheme()
+
+    return (
+        <Box>
+            <Box
+                sx={{
+                    px: theme.spacing(4),
+                    py: theme.spacing(2),
+                    backgroundColor: '#003A9B',
+                }}
+            >
+                <Navbar />
+            </Box>
+            <Box sx={{ mx: theme.spacing(4) }}>
+                <Container maxWidth="xl" disableGutters>
+                    <Typography variant="h2">test</Typography>
+                </Container>
+            </Box>
+            <FooterSection />
+        </Box>
+    )
+}
