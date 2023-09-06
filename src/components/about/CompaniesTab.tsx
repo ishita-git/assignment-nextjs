447c834bed4f2cca1c5fefc50448a2a04b9a1528
goyalkaran2001 <goyalkaran2001@gmail.com>
2023-09-07 01:09:18 +0530

    feat: mission and vision tab

--- src/components/about/CompaniesTab.tsx
+++ src/components/about/CompaniesTab.tsx
@@ -1,14 +1,14 @@
-import { Container, Typography, Box } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { companiesTabData } from "../../data/aboutData"
-import Image, { StaticImageData } from 'next/image'
+import  CompanyInfoCard  from './CompanyInfoCard'
 
 
 export default function CompaniesTab() {
     const theme = useTheme()
 
     return (
-        <Container maxWidth="xl">
+        <Box>
             <Box >
                 <Typography
                     variant="h2"
@@ -35,38 +35,7 @@ export default function CompaniesTab() {
                         : <></>
                 ))}
             </Box>
-        </Container>
-    )
-}
-
-
-function CompanyInfoCard({ title, des, coverImage }: { title: String, des: String, coverImage: string | StaticImageData }) {
-    const theme = useTheme();
-    return (
-        <Box sx={{ position: "relative", mb: "1.5rem" }} >
-            <Image
-                src={coverImage}
-                alt="card background"
-                style={{
-                    width: '100%',
-                    height: 'auto',
-
-                }}
-            />
-            <Box sx={{
-                p: theme.spacing(2.5),
-                top: 0,
-                left: 0,
-                width: "60%",
-                position: "absolute"
-            }}>
-                <Typography variant="h3" sx={{ textAlign: "start", mb: "1.5rem" }} >
-                    {title}
-                </Typography>
-                <Typography paragraph variant='body1' sx={{ lineHeight: "1.5rem", fontWeight: "400", color: "#FFFFFF", textAlign: "start" }}>
-                    {des}
-                </Typography>
-            </Box>
         </Box>
     )
 }
+
