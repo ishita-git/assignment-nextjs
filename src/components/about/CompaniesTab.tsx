12c12c97be11ee798dc1b547a94c20a392005162
goyalkaran2001 <goyalkaran2001@gmail.com>
2023-09-06 23:52:52 +0530

    feat: companies tab

--- src/components/about/CompaniesTab.tsx
+++ src/components/about/CompaniesTab.tsx
@@ -1,18 +1,72 @@
-import { Container, Typography } from '@mui/material'
+import { Container, Typography, Box } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+import { companiesTabData } from "../../data/aboutData"
+import Image, { StaticImageData } from 'next/image'
+
 
 export default function CompaniesTab() {
     const theme = useTheme()
 
     return (
         <Container maxWidth="xl">
-            <Typography
-                variant="h2"
-                textAlign="start"
-                sx={{ my: theme.spacing(2) }}
-            >
-                India
-            </Typography>
+            <Box >
+                <Typography
+                    variant="h2"
+                    textAlign="start"
+                    sx={{ my: theme.spacing(2) }}
+                >
+                    India
+                </Typography>
+                {companiesTabData.map((item, index) => (
+                    item.country == 'INDIA' ? <CompanyInfoCard des={item.des} coverImage={item.image} title={item.name} key={index} />
+                        : <></>
+                ))}
+            </Box>
+            <Box >
+                <Typography
+                    variant="h2"
+                    textAlign="start"
+                    sx={{ margin: "4rem 0 2rem 0" }}
+                >
+                    Overseas
+                </Typography>
+                {companiesTabData.map((item, index) => (
+                    item.country != 'INDIA' ? <CompanyInfoCard des={item.des} coverImage={item.image} title={item.name} key={index} />
+                        : <></>
+                ))}
+            </Box>
         </Container>
+    )
+}
+
+
+function CompanyInfoCard({ title, des, coverImage }: { title: String, des: String, coverImage: string | StaticImageData }) {
+    const theme = useTheme();
+    return (
+        <Box sx={{ position: "relative", mb: "1.5rem" }} >
+            <Image
+                src={coverImage}
+                alt="card background"
+                style={{
+                    width: '100%',
+                    height: 'auto',
+
+                }}
+            />
+            <Box sx={{
+                p: theme.spacing(2.5),
+                top: 0,
+                left: 0,
+                width: "60%",
+                position: "absolute"
+            }}>
+                <Typography variant="h3" sx={{ textAlign: "start", mb: "1.5rem" }} >
+                    {title}
+                </Typography>
+                <Typography paragraph variant='body1' sx={{ lineHeight: "1.5rem", fontWeight: "400", color: "#FFFFFF", textAlign: "start" }}>
+                    {des}
+                </Typography>
+            </Box>
+        </Box>
     )
 }
