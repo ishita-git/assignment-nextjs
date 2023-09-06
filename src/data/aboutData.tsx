43a7fd0930a2827b6412d8d36f1b4c90bf5a1a66
goyalkaran2001 <goyalkaran2001@gmail.com>
2023-09-06 21:43:42 +0530

    feat: about tab

---
+++ src/data/aboutData.tsx
@@ -0,0 +1,21 @@
+
+export const qualityPolicyData = [
+    "Providing comprehensive education and training to all our employees.",
+    "Implementing the ISO 9001:2015 Quality Management System to ensure excellence.",
+    "Swiftly addressing customer complaints and taking preventive actions.",
+    "Conducting internal audits to drive improvements and implement necessary corrections.",
+    "Adhering to all statutory and regulatory obligations.",
+    "Conducting regular management reviews to ensure alignment and progress.",
+    "Identifying potential risks and implementing mitigation strategies.",
+]
+
+export const keyAttributesData = [
+    "Dependable and Trustworthy Ally",
+    "Exceptional partnerships with global forwarders and local authorities",
+    "A team of skilled, seasoned, and dedicated professionals",
+    "Creative tailor-made solutions for your cargo logistics",
+    "Worldwide network of strategically placed offices",
+    "Committed to precise, punctual, and cost-effective delivery",
+    "Robust IT system linking vendors/customers for swift response",
+
+]
