cc5a4e612dd021f1a812c9169150d860329cb0e1
goyalkaran2001 <goyalkaran2001@gmail.com>
2023-10-02 14:36:49 +0530

    feat: companies about pages

---
+++ src/components/companies/StatisticsCard.tsx
@@ -0,0 +1,77 @@
+import { Box, Grid, Typography } from "@mui/material"
+
+
+export default function StatisticsCard({ exporterRank, importerRank, tradeBalanceRank, totalTradeAmtList }: { exporterRank: String, importerRank: String, tradeBalanceRank: String, totalTradeAmtList: string[] }) {
+    return <Box>
+        <Box sx={{ bgcolor: "#FFFFFF", p: "1.25rem", borderRadius: "1.25rem" }}>
+            <Grid container spacing={2} sx={{ mb: "2rem" }}>
+                <Grid item xs={12} sm={4}>
+                    <Box sx={{ bgcolor: "#EFF6FF", borderRadius: "1.5rem", p: "1.25rem", }} >
+                        <Typography variant="h4" sx={{ color: '#031225', textAlign: "start" }}>
+                            Exporter Rank
+                        </Typography >
+                        <Box sx={{ display: "flex", alignItems: "baseline", justifyContent: "center" }}>
+                            <Typography variant="h1" sx={{ color: '#031225' }}>{exporterRank}</Typography>
+                            <Typography variant="body1" sx={{ color: '#031225' }}>/138</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Box sx={{ bgcolor: "#EFF6FF", borderRadius: "1.5rem", p: "1.25rem", }} >
+                        <Typography variant="h4" sx={{ color: '#031225', textAlign: "start" }}>
+                            Importer Rank
+                        </Typography >
+                        <Box sx={{ display: "flex", alignItems: "baseline", justifyContent: "center" }}>
+                            <Typography variant="h1" sx={{ color: '#031225' }}>{importerRank}</Typography>
+                            <Typography variant="body1" sx={{ color: '#031225' }}>/138</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Box sx={{ bgcolor: "#EFF6FF", borderRadius: "1.5rem", p: "1.25rem", }} >
+                        <Typography variant="h4" sx={{ color: '#031225', textAlign: "start" }}>
+                            Trade Balance Rank
+                        </Typography >
+                        <Box sx={{ display: "flex", alignItems: "baseline", justifyContent: "center" }}>
+                            <Typography variant="h1" sx={{ color: '#031225' }}>{tradeBalanceRank}</Typography>
+                            <Typography variant="body1" sx={{ color: '#031225' }}>/138</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+            <Typography variant='h3' sx={{ textAlign: 'start', mb: '1rem', color: '#1B1B1F' }}>
+                Total Trade
+            </Typography>
+            <Box>
+                <Grid container spacing={1} justifyContent={"space-between"}>
+                    <Grid item>
+                        {totalTradeList.map((item, index) => (
+                            <Typography key={index} variant="body1" textAlign={"start"} sx={{ color: '#031225', mb: "0.5rem" }}>
+                                {item}
+                            </Typography>
+                        ))}
+                    </Grid>
+                    <Grid item>
+                        {totalTradeAmtList.map((item, index) => (
+                            <Typography key={index} variant="body1" textAlign={"end"} sx={{ color: '#031225', mb: "0.5rem" }}>
+                                {item}
+                            </Typography>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+        <Typography variant='h3' sx={{ my: '2rem', color: '#1B1B1F' }}>
+            Trade Statistics
+        </Typography>
+    </Box>
+
+}
+
+const totalTradeList = [
+    "Total Exports (2021)",
+    "Total Imports (2021)",
+    "Total Balance (2021)",
+    "Exports of goods and services (2022)",
+    "Imports of goods and services (2022)",
+]
--- src/pages/companies/container-lines/index.tsx
+++ src/pages/companies/container-lines/index.tsx
@@ -1,22 +1,51 @@
 import React from 'react'
-import Image from 'next/image'
-import { Box, Container, Typography } from '@mui/material'
+import Image, { StaticImageData } from 'next/image'
+import { Box, Container, Grid, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import CompaniesLayout from '../CompaniesLayout'
 import ServicesCards from '../../../components/services/ServicesCards'
 import containerLinesBg from '../../../assets/companies/container-lines-bg.png'
 import processBanner from '../../../assets/companies/process-banner.png'
 import branchesMap from '../../../assets/companies/branches-map.png'
+import insulatedContainer from '../../../assets/companies/muskan_conatiners/insulated_containers.png'
+import openTopContainer from '../../../assets/companies/muskan_conatiners/open_to_containers.png'
+import bulkContainer from '../../../assets/companies/muskan_conatiners/bult-containers.png'
+import tankContainer from '../../../assets/companies/muskan_conatiners/tank-containers.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Keyboard, Navigation, Pagination } from 'swiper/modules'
+import { ProcessData, OfficeLoactionIndia, OfficeLoactionInternational } from '../../../data/companiesData'
+import StatisticsCard from '@/components/companies/StatisticsCard'
+import CustomText from '@/components/companies/CustomText'
+
 
 function ProcessSection() {
     return (
         <Box sx={{ position: 'relative' }}>
             <Image src={processBanner} alt='process banner' style={{ width: '100%', height: 'auto' }} />
-            <Box sx={{ position: 'absolute', top: 0, width: '100%' }}>
+            <Box sx={{ position: 'absolute', top: 0, width: '100%', px: "5rem" }}>
                 <Typography variant='h3' sx={{ my: '3rem' }}>
                     Process
                 </Typography>
-                add cards here
+                <Box sx={{display:"flex"}}>
+                {ProcessData.map((item, index) => (
+                <Box key={index} sx={{ height: '20rem', bgcolor: "#FFFFFF", borderRadius: "1rem", padding: '1.25rem' ,mx:"1rem"}}>
+                    <Image
+                        src={item.image}
+                        alt='card background'
+                        style={{
+                            width: '25%',
+                            height: 'auto',
+                        }}
+                    />
+                    <Typography variant='h6' textAlign={'center'} sx={{ fontWeight: "bold", color: '#031225', my: '1rem' }}>
+                        {item.title}
+                    </Typography>
+                    <Typography variant='body1' textAlign={'center'} sx={{ color: '#031225', marginBottom: '1rem' }}>
+                        {item.desc}
+                    </Typography>
+                </Box>
+        ))}
+                </Box>
             </Box>
         </Box>
     )
@@ -32,9 +61,72 @@ export default function Home() {
                     <Typography variant='h3' sx={{ textAlign: 'start', mb: '1rem', color: '#1B1B1F' }}>
                         About Us
                     </Typography>
-                    put content here
+                    <CustomText text="Muskan Container Lines Pvt. Ltd. is a prominent Indian container operator, boasting the largest container fleet in India and the Indian subcontinent. With a unique track record in efficient fleet management across more than 300,000 routes domestically and internationally, we specialize in door-to-door integrated transportation and logistics solutions. These capabilities enable us to deliver container cargo to various destinations in India, the CIS, Europe, or Asia, using both in-house transportation assets and strategic partner collaborations. Our network of offices throughout the Indian subcontinent is seamlessly connected through a unified information system, ensuring efficient operations." />
+                    <Grid container spacing={2} sx={{ mb: "2rem" }}>
+                        <Grid item xs={12} sm={6}>
+                            <Typography textAlign={'start'} variant='h4' sx={{ fontWeight:"bold",color: '#031225', marginBottom: '1rem' }}>
+                                Offices in India
+                            </Typography>
+                            <Typography
+                                component={'ul'}
+                                textAlign={'start'}
+                                variant='body1'
+                                sx={{ color: '#031225', ml: '1.25rem' }}
+                            >
+                                {OfficeLoactionIndia.map((item, index) => (
+                                    <Typography
+                                        component={'li'}
+                                        textAlign={'start'}
+                                        variant='body1'
+                                        sx={{ color: '#031225', mb: '1rem' }}
+                                        key={index}
+                                    >
+                                        {item}
+                                    </Typography>
+                                ))}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Typography textAlign={'start'} variant='h4' sx={{fontWeight:"bold", color: '#031225', marginBottom: '1rem' }}>
+                                International offices
+                            </Typography>
+                            <Typography
+                                component={'ul'}
+                                textAlign={'start'}
+                                variant='body1'
+                                sx={{ color: '#031225', ml: '1.25rem' }}
+                            >
+                                {OfficeLoactionInternational.map((item, index) => (
+                                    <Typography
+                                        component={'li'}
+                                        textAlign={'start'}
+                                        variant='body1'
+                                        sx={{ color: '#031225', mb: '1rem' }}
+                                        key={index}
+                                    >
+                                        {item}
+                                    </Typography>
+                                ))}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <CustomText text="Shipping cargo often requires specialized containers to ensure safe and efficient transportation. Our company provides a variety of specialized containers tailored to different cargo needs. These include:" />
+                    <Grid container spacing={1} sx={{ my: "1.5rem" }}>
+                        <Grid item xs={12} sm={6}>
+                            <ContainerType title="Insulated Containers" desc="Ideal for perishable goods, these containers maintain precise temperatures, keeping your cargo fresh." image={insulatedContainer} />
+                            <ContainerType title="Bulk Containers" desc="Perfect for transporting large quantities of loose cargo, such as grains or minerals." image={bulkContainer} />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <ContainerType title="Open Top Containers" desc="Designed for oversize cargo, these containers have removable tops for easy loading and unloading." image={openTopContainer} />
+                            <ContainerType title="Tank Containers" desc="Specifically designed for liquid and chemical cargo, ensuring safe containment and transport." image={tankContainer} />
+                        </Grid>
+                    </Grid>
+                    <Typography variant='h3' sx={{ textAlign: 'start', mt:"4rem",mb: '2rem', color: '#1B1B1F' }}>
+                        Statistics
+                    </Typography>
+                    <StatisticsCard exporterRank="16" importerRank="10" tradeBalanceRank="136" totalTradeAmtList={totalTradeAmtList} />
                 </Container>
-            </Box>
+            </Box >
 
             <ProcessSection />
             <ServicesCards companiesPage />
@@ -46,6 +138,32 @@ export default function Home() {
                     <Image src={branchesMap} alt='branches' style={{ width: '40rem', height: 'auto' }} />
                 </Container>
             </Box>
-        </CompaniesLayout>
+        </CompaniesLayout >
     )
 }
+
+
+function ContainerType({ image, title, desc }: { image: string | StaticImageData, title: String, desc: String }) {
+    return <Box sx={{ mb: "2rem" }}>
+        <Grid container alignItems="center" >
+            <Grid item xs={4}>
+                <Image src={image} alt="" style={{ height: 'auto', width: "50%", }} />
+            </Grid>
+            <Grid item xs={8}>
+                <Typography variant='h6' textAlign={'start'} sx={{ fontWeight: "bold", color: '#031225', marginBottom: '1rem' }}>
+                    {title}
+                </Typography>
+                <Typography variant='body1' textAlign={'start'} sx={{ color: '#031225', marginBottom: '1rem' }}>
+                    {desc}
+                </Typography>
+            </Grid>
+        </Grid>
+    </Box>
+}
+const totalTradeAmtList = [
+    "$394,813,673,347",
+    "$570,402,004,491",
+    "$175,588,331,144",
+    "22.45%",
+    "26.92%",
+]
