c61cdb2e10d949d2d14c7ff175c62499d760422b
Satyen-Sen <Satyendra.sant7@gmail.com>
2023-10-28 15:19:34 +0530

    caraoual - fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { useTheme } from '@mui/material/styles'
 import { AppBar, Box, Container, Grid, Toolbar, Typography, useScrollTrigger } from '@mui/material'
 import HeaderSection from '../sections/HeaderSection'
--- src/sections/ClienteleSection.tsx
+++ src/sections/ClienteleSection.tsx
@@ -1,85 +1,3 @@
-// import React, { useRef, useState } from 'react'
-// import Image from 'next/image'
-// import { Box, Container, Grid, Typography } from '@mui/material'
-// import { useTheme } from '@mui/material/styles'
-// import { clientsData } from '../data/clientsData'
-// import ClienteleCard from '../components/ClienteleCard'
-// import ClientIconCard from '../components/ClientIconCard'
-// // swiper
-// import { Swiper, SwiperSlide } from 'swiper/react'
-// import { Autoplay, Keyboard, FreeMode, Navigation, Thumbs, Pagination } from 'swiper/modules'
-// import 'swiper/css'
-// import 'swiper/css/pagination'
-// import 'swiper/css/free-mode'
-// import 'swiper/css/navigation'
-// import 'swiper/css/thumbs'
-
-// export default function ClienteleSection() {
-//     const theme = useTheme()
-//     const [thumbsSwiper, setThumbsSwiper] = useState<null>(null)
-
-//     return (
-//         <Box sx={{ p: theme.spacing(4) }}>
-//             <Typography variant='h2'>Appreciated by some of the best in industry</Typography>
-//             <Container maxWidth='md' disableGutters sx={{ py: theme.spacing(2), position: 'relative' }}>
-//                 <Box sx={{ my: theme.spacing(2) }}>
-//                     <Swiper
-//                         loop={true}
-//                         spaceBetween={10}
-//                         navigation={true}
-//                         thumbs={{ swiper: thumbsSwiper }}
-//                         modules={[FreeMode, Navigation, Thumbs]}
-//                         className='mySwiper2'
-//                     >
-//                         {clientsData.map((item) => (
-//                             <SwiperSlide key={item.id}>
-//                                 <Box
-//                                     sx={{
-//                                         width: '100%',
-//                                         display: 'flex',
-//                                         justifyContent: 'center',
-//                                     }}
-//                                 >
-//                                     <ClienteleCard
-//                                         cardImage={item.cardImage}
-//                                         cardIcon={item.cardIcon}
-//                                         title={item.title}
-//                                         subtitle={item.subtitle}
-//                                     />
-//                                 </Box>
-//                             </SwiperSlide>
-//                         ))}
-//                     </Swiper>
-//                     <Swiper
-//                         onSwiper={setThumbsSwiper}
-//                         loop={true}
-//                         spaceBetween={10}
-//                         slidesPerView={4}
-//                         freeMode={true}
-//                         watchSlidesProgress={true}
-//                         modules={[FreeMode, Thumbs]}
-//                         className='mySwiper'
-//                     >
-//                         {clientsData.map((item) => (
-//                             <SwiperSlide key={item.id}>
-//                                 <Box
-//                                     sx={{
-//                                         width: '100%',
-//                                         display: 'flex',
-//                                         justifyContent: 'center',
-//                                     }}
-//                                 >
-//                                     <ClientIconCard icon={item.cardIcon} />
-//                                 </Box>
-//                             </SwiperSlide>
-//                         ))}
-//                     </Swiper>
-//                 </Box>
-//             </Container>
-//         </Box>
-//     )
-// }
-
 import React, { useState } from 'react'
 import Image from 'next/image'
 import { Box, Container, Fade, IconButton, Typography } from '@mui/material'
@@ -95,6 +13,7 @@ export default function ClienteleSection() {
     const [visibleIcons, setVisibleIcons] = useState(visibleIconSet)
     const [startIconIndex, setStartIconIndex] = useState(0)
     const [cardIndex, setCardIndex] = useState(startIconIndex)
+    const [checked, setChecked] = useState(false)
 
     const handlePrev = () => {
         if (startIconIndex > 0) {
@@ -112,6 +31,11 @@ export default function ClienteleSection() {
         }
     }
 
+    const changeCard = (index: number) => {
+        setCardIndex(startIconIndex + index)
+        setChecked((prev) => !prev)
+    }
+
     return (
         <Box sx={{ p: theme.spacing(4) }}>
             <Typography variant='h2'>Appreciated by some of the best in industry</Typography>
@@ -123,13 +47,15 @@ export default function ClienteleSection() {
                         justifyContent: 'center',
                     }}
                 >
-                    <Fade in={true} timeout={500}>
-                        <ClienteleCard
-                            cardImage={clientsData?.[cardIndex]?.cardImage}
-                            cardIcon={clientsData?.[cardIndex]?.cardIcon}
-                            title={clientsData?.[cardIndex]?.title}
-                            subtitle={clientsData?.[cardIndex]?.subtitle}
-                        />
+                    <Fade in={checked} timeout={2000}>
+                        <div>
+                            <ClienteleCard
+                                cardImage={clientsData?.[cardIndex]?.cardImage}
+                                cardIcon={clientsData?.[cardIndex]?.cardIcon}
+                                title={clientsData?.[cardIndex]?.title}
+                                subtitle={clientsData?.[cardIndex]?.subtitle}
+                            />
+                        </div>
                     </Fade>
                 </Box>
             </Container>
@@ -147,11 +73,7 @@ export default function ClienteleSection() {
                         <WestRoundedIcon fontSize='large' sx={{ color: theme.palette.primary.main }} />
                     </IconButton>
                     {clientsData.slice(startIconIndex, startIconIndex + visibleIcons).map((item, index) => (
-                        <Box
-                            sx={{ height: '4rem', display: 'flex' }}
-                            key={index}
-                            onClick={() => setCardIndex(startIconIndex + index)}
-                        >
+                        <Box sx={{ height: '4rem', display: 'flex' }} key={index} onClick={() => changeCard}>
                             <Image
                                 src={item.cardIcon}
                                 alt='client icon'
